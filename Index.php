27999a8144d2d0c0236e5c169a1134f4e378bc38
MeeLucky <44944755+MeeLucky@users.noreply.github.com>
2025-05-16 04:06:21 +0300

    Init

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<title>Nom-nom and calculate</title>
+</head>
+<body>
+	<?php echo "testers"; ?>
+	<script src="scripts/script.js"></script>
+</body>
+</html>
